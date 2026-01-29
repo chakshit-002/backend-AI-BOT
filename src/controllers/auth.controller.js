aa600b397098160ab28bc4ce7d081ee98b5adcd1
chakshit-002 <gunidiachakshit@gmail.com>
2026-01-29 19:13:55 +0530

    samesite error-4

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -92,8 +92,9 @@ async function logoutUser(req, res) {
         res.cookie("token", "", {
             httpOnly: true,
             expires: new Date(0),
-            sameSite: "none",
+            sameSite: "None",
             secure: true,
+            path:"/"
         })
         res.status(200).json({
             success: true,
