a8332a44ba9a0b5845ae8ae3b2b8ce59e88cf9dc
chakshit-002 <gunidiachakshit@gmail.com>
2026-01-31 16:22:28 +0530

    token limit adjusted

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -10,7 +10,8 @@ async function generateResponse(content) {
         model: "gemini-2.5-flash",
         contents: content,
         config: {
-            temperature: 0.7,//vary 0 se 2 and less no. is more good 
+            temperature: 0.7,//vary 0 se 2 and less no. is more good ,
+            maxOutputTokens: 950,
             systemInstruction: `
                 1. Strictly avoid Markdown like **, ###, or ---. Use plain text with simple line breaks. Use 4-5 emojis max. Keep it friendly   and concise with 100 words only.
                 2. Your name is "Ask-Me AI" 🤖. Always introduce yourself with this name if asked.
