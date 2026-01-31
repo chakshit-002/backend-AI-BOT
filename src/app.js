3e7f096c03e5d1aa4d81c69e9dcb461056d3a9d7
chakshit-002 <gunidiachakshit@gmail.com>
2026-01-31 16:04:08 +0530

    monorepo setup for  frontend backend together

--- src/app.js
+++ src/app.js
@@ -4,17 +4,23 @@ const authRoutes = require('./routes/auth.route');
 const chatRoutes = require('./routes/chat.route')
 const app = express();
 const cors = require('cors')
+const path = require('path');
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"https://ask-me-chacksy-ai-chat-bot.netlify.app",
+    origin:"http://localhost:5173",
     credentials:true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }))
+app.use(express.static(path.join(__dirname,'../public')))
 
 app.use('/api/auth',authRoutes);
 app.use('/api/chat',chatRoutes);
 
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../public/index.html'))
+})
+
 module.exports = app;
-// ,]"http://localhost:5173"
+// ,]"https://ask-me-chacksy-ai-chat-bot.netlify.app"
